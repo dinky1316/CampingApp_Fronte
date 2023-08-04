864d6f9554560dc8b68b4c7ffd72ab082df02616
JMY00n <3zlll0@naver.com>
2023-08-04 18:05:19 +0900

    데이터 저장이 안 됨. 수정 필요

--- campingapp/build.gradle.kts
+++ campingapp/build.gradle.kts
@@ -1,8 +1,10 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
+    id ("kotlin-kapt")
     // Add the dependency for the Google services Gradle plugin
-    id("com.google.gms.google-services") version "4.3.15" apply false
+//    id("com.google.gms.google-services") version "4.3.15" apply false
 //    id 'kotlin-kapt'
 
 }
@@ -18,9 +20,9 @@ android {
         versionCode = 1
         versionName = "1.0"
 
-//        testInstrumentationRunner "androidx.test.runner.AndroidJUnitRunner"
-//        // dex 파일 갯수 제한을 풀어주는 코드.
-//        multiDexEnabled true
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        // dex 파일 갯수 제한을 풀어주는 코드.
+        multiDexEnabled = true
     }
 
     buildTypes {
@@ -51,21 +53,19 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.2.0"))
     implementation("com.google.firebase:firebase-analytics-ktx")
 
-    // second add
-//    implementation 'com.google.android.gms:play-services-auth:19.2.0'
-//    implementation 'androidx.multidex:multidex:2.0.1'
-//    implementation 'com.google.firebase:firebase-auth-ktx:21.0.1'
-//    implementation 'com.google.firebase:firebase-firestore-ktx:24.0.0'
-//    implementation 'com.google.firebase:firebase-storage-ktx:20.0.0'
-//    implementation 'com.github.bumptech.glide:glide:4.12.0'
-//    implementation 'com.firebaseui:firebase-ui-storage:8.0.0'
-//    implementation 'com.github.bumptech.glide:compiler:4.12.0'
-//    implementation 'com.google.firebase:firebase-messaging-ktx:23.0.0'
+    implementation ("androidx.multidex:multidex:2.0.1")
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    implementation("com.github.bumptech.glide:compiler:4.12.0")
+
+
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-firestore-ktx:24.7.0")
+    implementation("com.firebaseui:firebase-ui-storage:8.0.0")
+    implementation("com.google.firebase:firebase-storage-ktx:20.2.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- campingapp/src/main/java/com/camp/campingapp/MainActivity.kt
+++ campingapp/src/main/java/com/camp/campingapp/MainActivity.kt
@@ -3,7 +3,12 @@ package com.camp.campingapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.camp.campingapp.databinding.ActivityMainBinding
+import com.camp.campingapp.model.ItemData
+import com.camp.campingapp.recycler.MyAdapter
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -17,5 +22,24 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this, WriteActivity::class.java))
         }
 
+    }
+
+    private fun makeRecyclerView() {
+        MyApplication.db.collection("news").get()
+            .addOnSuccessListener { result ->
+                val itemList = mutableListOf<ItemData>()
+                for (document in result) {
+                    val item = document.toObject(ItemData::class.java)
+                    itemList.add(item)
+                }
+                binding.mainRecyclerView.layoutManager = LinearLayoutManager(this)
+                binding.mainRecyclerView.adapter = MyAdapter(this, itemList)
+            }
+            .addOnFailureListener{exception ->
+                Log.d("kkang", "error.. getting document..", exception)
+                Toast.makeText(this, "서버 데이터 획득 실패", Toast.LENGTH_SHORT).show()
+
+
+            }
     }
 }
